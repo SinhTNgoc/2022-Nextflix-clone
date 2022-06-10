02fc5f9244406e988c09640b58f1337fe9d5a688
SinhTNgoc <tranngocsinh2211@gmail.com>
2022-06-10 10:37:08 +0700

    netflix ui-home + navbar

---
+++ netflix/src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss";
+import Home from "./pages/home/Home";
+// import Register from "./pages/register/Register"
+// import Login from "./pages/login/Login"
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Login/> */}
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
