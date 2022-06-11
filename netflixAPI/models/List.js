3f20d3c4e3fadededbdca396a96ac1bc647c7774
SinhTNgoc <tranngocsinh2211@gmail.com>
2022-06-11 12:29:48 +0700

    server

---
+++ netflixAPI/models/List.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const listSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    type: {
+      type: String,
+    },
+    genre: {
+      type: String,
+    },
+    content: {
+      type: Array,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("List", listSchema);
